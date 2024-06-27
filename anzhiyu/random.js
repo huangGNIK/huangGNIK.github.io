066b6f7113a80109efa6a6d252d3348ca0845693
huangGNIK <2682550792@qq.com>
2024-06-27 19:51:16 +0800

    Site updated: 2024-06-27 19:51:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/24/hello-world/","2024/06/25/第一篇博客/"];function toRandomPost(){
+var posts=["2024/06/24/hello-world/","2024/06/27/今日上线内容/","2024/06/25/第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
